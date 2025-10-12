c2c066010f13966123e40be94bbfb1857c27a172
Laurane-lab <83465828+Laurane-lab@users.noreply.github.com>
2025-10-12 14:44:45 +0000

    Indexbis devient index principal

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Index from "./pages/Index";
-import IndexBis from "./pages/IndexBis";
 import NotFound from "./pages/NotFound";
 import Connexion from "./pages/Connexion";
 import GameExplanation from "./pages/GameExplanation";
@@ -26,7 +25,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/indexbis" element={<IndexBis />} />
           <Route path="/connexion" element={<Connexion />} />
           <Route path="/profil" element={
             <ProtectedRoute>
